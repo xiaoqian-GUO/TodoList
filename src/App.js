529135df2904398807831e128bf7adccd50cf298
xiaoqian-GUO <1454135606@qq.com>
2018-10-06 23:46:40 +0800

    commit code by create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './css/App.css';
+import TextInput from "./todolist";
+
+class App extends Component {
+  render() {
+    return (
+        <TextInput />
+    );
+  }
+}
+
+export default App;
